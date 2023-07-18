93cd1e703ff9301158dd685414d530ac68c3edc2
RudiJr <rudi.juniior@gmail.com>
2023-07-18 11:58:46 -0300

    Created criaNegociacao()

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -12,12 +12,15 @@ export class NegociacaoController {
     };
 
     adiciona() {
+        const negociacao = this.criaNegociacao
+        console.log(negociacao);
+    };
+
+    criaNegociacao() {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
-        const negociacao = new Negociacao(date, quantidade, valor);
- 
-       console.log(negociacao);
-    };
+        return new Negociacao(date, quantidade, valor);
+    }; 
 }; 
