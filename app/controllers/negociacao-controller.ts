264423e3491072ae470f269550d312a9b401ff9d
RudiJr <rudi.juniior@gmail.com>
2023-07-31 17:36:12 -0300

    Add negociacoes in adiciona method

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement; 
     private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
 
     constructor() {
         this.inputData = document.querySelector('#data');
@@ -12,11 +14,12 @@ export class NegociacaoController {
     };
 
     adiciona(): void {
-        const negociacao = this.criaNegociacao
-        console.log(negociacao);
+        const negociacao = this.criaNegociacao();
+        this.negociacoes.adiciona(negociacao);
+        console.log(this.negociacoes.lista());
         this.limparFormulario();
     };
-
+        
     criaNegociacao(): Negociacao {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
