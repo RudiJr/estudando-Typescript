40b762c0b1df0972fd38cd18c94265f40bf795f3
RudiJr <rudi.juniior@gmail.com>
2023-07-31 17:05:54 -0300

    Updated compiler

--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -2,5 +2,13 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = [];
     }
+    adiciona(negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+    ;
+    lista() {
+        return this.negociacoes;
+    }
+    ;
 }
 ;
