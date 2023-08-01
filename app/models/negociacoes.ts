79a5f8bb2f7612f6813f9ed5fbfa95bdadcb8e5b
RudiJr <rudi.juniior@gmail.com>
2023-08-01 15:32:47 -0300

    Updated arrays

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,16 +1,16 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao){
         this.negociacoes.push(negociacao);
     };
 
-    lista(): ReadonlyArray<Negociacao> {
+    lista(): readonly Negociacao[] {
         return this.negociacoes;
     }; /* The `ReadonlyArray` type in TypeScript is used to create an immutable array. It means
     that once the array is created, its elements cannot be modified or added. It provides
     read-only access to the array's elements, preventing any modifications to the array
     itself. */
-};
+}; 
