7e18723ff2a0cd2b1f8f02caf6bb61e7d38f2931
RudiJr <rudi.juniior@gmail.com>
2023-08-01 15:37:22 -0300

    Updated  constructor

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,28 +1,12 @@
 export class Negociacao {
-   /* The `private` keyword is used to declare a private member in a class. It restricts access to the
-   member only within the class itself. In the given code, the `_data`, `_quantidade`, and `_valor`
-   variables are declared as private, which means they can only be accessed within the `Negociacao`
-   class. */
-
+  
     constructor(
-        private _data: Date, 
-        private _quantidade: number, 
-        private _valor: number
+        public readonly data: Date, 
+        public readonly quantidade: number, 
+        public readonly valor: number
     ){};
 
-    get data(): Date {
-        return this._data
-    };
-
-    get quantidade(): number { 
-        return this._quantidade
-    };
-
-    get valor(): number {
-        return this._valor
-    };
-
     get volume(): number {
-        return this._quantidade * this._valor
+        return this.quantidade * this.valor
     };
 };
