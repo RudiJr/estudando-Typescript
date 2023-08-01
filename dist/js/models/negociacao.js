4b46b0e7c009c5e43b6f41f12f9c8b6ddc0aff8f
RudiJr <rudi.juniior@gmail.com>
2023-08-01 15:37:30 -0300

    updated compiler

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,28 +1,12 @@
 export class Negociacao {
-    /* The `private` keyword is used to declare a private member in a class. It restricts access to the
-    member only within the class itself. In the given code, the `_data`, `_quantidade`, and `_valor`
-    variables are declared as private, which means they can only be accessed within the `Negociacao`
-    class. */
-    constructor(_data, _quantidade, _valor) {
-        this._data = _data;
-        this._quantidade = _quantidade;
-        this._valor = _valor;
-    }
-    ;
-    get data() {
-        return this._data;
-    }
-    ;
-    get quantidade() {
-        return this._quantidade;
-    }
-    ;
-    get valor() {
-        return this._valor;
+    constructor(data, quantidade, valor) {
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     ;
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
     ;
 }
