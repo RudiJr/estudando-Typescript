55a7c12da6f0ef369f792b33c7d25dd11faa78e5
RudiJr <rudi.juniior@gmail.com>
2023-08-01 10:56:42 -0300

    Add ReadonlyArray to create an immutable array

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -7,7 +7,10 @@ export class Negociacoes {
         this.negociacoes.push(negociacao);
     };
 
-    lista(): Array<Negociacao> {
+    lista(): ReadonlyArray<Negociacao> {
         return this.negociacoes;
-    };
+    }; /* The `ReadonlyArray` type in TypeScript is used to create an immutable array. It means
+    that once the array is created, its elements cannot be modified or added. It provides
+    read-only access to the array's elements, preventing any modifications to the array
+    itself. */
 };
