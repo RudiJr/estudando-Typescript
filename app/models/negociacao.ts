5cd7843b6c0450330d1e814ba369102e771db141
RudiJr <rudi.juniior@gmail.com>
2023-08-01 15:08:17 -0300

    Updated constructor

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -3,15 +3,12 @@ export class Negociacao {
    member only within the class itself. In the given code, the `_data`, `_quantidade`, and `_valor`
    variables are declared as private, which means they can only be accessed within the `Negociacao`
    class. */
-    private _data: Date; 
-    private _quantidade: number;
-    private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    };
+    constructor(
+        private _data: Date, 
+        private _quantidade: number, 
+        private _valor: number
+    ){};
 
     get data(): Date {
         return this._data
