5a0592355d9f9b709235b5d79eb7fa688e88f2fc
RudiJr <rudi.juniior@gmail.com>
2023-09-19 16:52:59 -0300

    turned view into a abstract class and updated template too

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,4 +1,4 @@
-export class View<T> {
+export abstract class View<T> {
     protected elemento: HTMLElement;
 
     constructor(seletor: string) {
@@ -9,9 +9,7 @@ export class View<T> {
         const template = this.template(model);
         this.elemento.innerHTML = template; 
     };
-    template(model: T): string{
-       throw Error('Classe filha precisa implementar o método template');
-    };
+    abstract template(model: T): string;
 };
 
 
