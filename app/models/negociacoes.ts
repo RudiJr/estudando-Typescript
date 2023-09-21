bcfce89165a72cee7a61b595556089432cb58c6d
RudiJr <rudi.juniior@gmail.com>
2023-09-21 15:21:52 -0300

    Adding methods to all files

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -3,11 +3,11 @@ import { Negociacao } from "./negociacao.js";
 export class Negociacoes {
     private negociacoes: Negociacao[] = [];
 
-    adiciona(negociacao: Negociacao){
+    public adiciona(negociacao: Negociacao){
         this.negociacoes.push(negociacao);
     };
 
-    lista(): readonly Negociacao[] {
+    public lista(): readonly Negociacao[] {
         return this.negociacoes;
     }; /* The `ReadonlyArray` type in TypeScript is used to create an immutable array. It means
     that once the array is created, its elements cannot be modified or added. It provides
