7e5823dcd5cc8dde33d7f0effd3725f73460e2aa
RudiJr <rudi.juniior@gmail.com>
2023-09-25 15:38:53 -0300

    updated compiler

--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -9,9 +9,6 @@ export class Negociacoes {
     lista() {
         return this.negociacoes;
     }
-    ; /* The `ReadonlyArray` type in TypeScript is used to create an immutable array. It means
-    that once the array is created, its elements cannot be modified or added. It provides
-    read-only access to the array's elements, preventing any modifications to the array
-    itself. */
+    ;
 }
 ;
